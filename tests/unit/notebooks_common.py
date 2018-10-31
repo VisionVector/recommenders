4c7f1a31653a558c5ba26c61451ee92f11083784
Markus Cozowicz <eisber@eisber.net>
2018-11-01 00:42:50 +0100

    trigger build

--- tests/unit/notebooks_common.py
+++ tests/unit/notebooks_common.py
@@ -1,6 +1,3 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
 import os
 
 # Unless manually modified, python3 should be the name of the current jupyter kernel
