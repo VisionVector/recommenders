e7e876097e44cd8c36280bb597c07ed33ee0f08e
Zhang Le <yueguoguo@users.noreply.github.com>
2018-11-28 14:43:22 +0800

    Merge pull request #293 from dciborow/patch-2

--- scripts/generate_conda_file.sh
+++ scripts/generate_conda_file.sh
@@ -24,7 +24,6 @@ CONDA_FILE="conda_bare.yaml"
 
 # default CPU-only no-pySpark versions of conda packages.
 tensorflow="tensorflow"
-gpu="#"
 pyspark="#"
 
 # flags to detect if both CPU and GPU are specified
@@ -40,7 +39,6 @@ do
 			exit
 			;;
 		--gpu)
-			gpu=""
 			tensorflow="tensorflow-gpu"
 			CONDA_FILE="conda_gpu.yaml"
 			gpu_flag=true
@@ -89,6 +87,7 @@ ${pyspark}- pyspark==2.3.1
 - jupyter>=1.0.0
 - fastparquet>=0.1.6
 ${pyspark}- pyarrow>=0.8.0
+${pyspark}- pybind11>=2.2
 - pip:
   - pandas>=0.22.0
   - scipy>=1.0.0
@@ -99,11 +98,8 @@ ${pyspark}- pyarrow>=0.8.0
   - pytest==3.6.4
   - papermill>=0.15.0
   - black>=18.6b4
+${pyspark}  - pysarplus>=0.2.2
   - memory-profiler>=0.54.0
-${gpu}  - numba>=0.38.1 
-  - gitpython>=2.1.8
-  - pydocumentdb>=2.3.3
-  - azureml-core>=0.1.74
 EOM
 
 echo "Conda file generated: " $CONDA_FILE
