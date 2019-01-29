c09c579f50e86627d876f71867e678f67c5a71e5
Ubuntu <gramhagen@users.noreply.github.com>
2019-01-29 15:13:55 +0000

    Merge branch 'staging' into gramhagen/vw_example

--- tests/integration/test_notebooks_gpu.py
+++ tests/integration/test_notebooks_gpu.py
@@ -19,76 +19,20 @@ def test_gpu_vm():
 @pytest.mark.integration
 @pytest.mark.gpu
 @pytest.mark.parametrize(
-    "size, epochs, expected_values",
+    "size, expected_values",
     [
-        ("1m", 10, {"map": 0.024821, "ndcg": 0.153396, "precision": 0.143046, "recall": 0.056590}),
-        #("10m", 5, {"map": 0.024821, "ndcg": 0.153396, "precision": 0.143046, "recall": 0.056590})# takes too long
+        ("1m", {"map": 0.024821, "ndcg": 0.153396, "precision": 0.143046, "recall": 0.056590}),
     ],
 )
-def test_ncf_integration(notebooks, size, epochs, expected_values):
+def test_ncf_integration(notebooks, size, expected_values):
     notebook_path = notebooks["ncf"]
     pm.execute_notebook(
         notebook_path,
         OUTPUT_NOTEBOOK,
         kernel_name=KERNEL_NAME,
-        parameters=dict(TOP_K=10, 
-                        MOVIELENS_DATA_SIZE=size,
-                        EPOCHS=epochs,
-                        BATCH_SIZE=512),
+        parameters=dict(TOP_K=10, MOVIELENS_DATA_SIZE=size),
     )
     results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
 
     for key, value in expected_values.items():
         assert results[key] == pytest.approx(value, rel=TOL)
-
-
-@pytest.mark.integration
-@pytest.mark.gpu
-@pytest.mark.parametrize(
-    "size, epochs, expected_values",
-    [
-        ("100k", 50, {"map": 0.051398, "ndcg": 0.204228, "precision": 0.183987, "recall": 0.105546,
-                  "map2": 0.049723, "ndcg2": 0.201361, "precision2": 0.180276, "recall2": 0.103631}),
-    ],
-)
-@pytest.mark.skip(reason="as of now, it takes too long to do a integration test")
-def test_ncf_deep_dive_integration(notebooks, size, epochs, expected_values):
-    notebook_path = notebooks["ncf_deep_dive"]
-    pm.execute_notebook(notebook_path, 
-                        OUTPUT_NOTEBOOK, 
-                        kernel_name=KERNEL_NAME,
-                        parameters=dict(TOP_K=10, 
-                                        MOVIELENS_DATA_SIZE=size,
-                                        EPOCHS=epochs,
-                                        BATCH_SIZE=512),
-                       )
-    results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
-
-    for key, value in expected_values.items():
-        assert results[key] == pytest.approx(value, rel=TOL)
-
-
-@pytest.mark.integration
-@pytest.mark.gpu
-@pytest.mark.parametrize(
-    "size, epochs, expected_values",
-    [
-        ("1m", 10, {"map": 0.025739, "ndcg": 0.183417, "precision": 0.167246, "recall": 0.054307,
-                "rmse": 0.881267, "mae": 0.700747, "rsquared": 0.379963, "exp_var": 0.382842}),
-        #("10m", 5, ), # it gets an OOM on pred = learner.model.forward(u, m)
-    ],
-)
-def test_fastai_integration(notebooks, size, epochs, expected_values):
-    notebook_path = notebooks["fastai"]
-    pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
-    pm.execute_notebook(
-        notebook_path,
-        OUTPUT_NOTEBOOK,
-        kernel_name=KERNEL_NAME,
-        parameters=dict(TOP_K=10, MOVIELENS_DATA_SIZE=size, EPOCHS=epochs),
-    )
-    results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
-    
-    for key, value in expected_values.items():
-        assert results[key] == pytest.approx(value, rel=TOL)
-
