ad89d4185b9e325ff7fe2368b831e363fc1638a5
Jianxun Lian <jialia@microsoft.com>
2019-01-30 11:31:14 +0800

    PR failed with 'notebooks_unit_tests_staging — #2249105 failed'. That is very weird, I didn't modify this file. Anyway I update this file by copying the latest code from the  PR

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@
 import calendar
 import datetime
 import os
-import numpy as np
 import pandas as pd
 import pytest
 from sklearn.model_selection import train_test_split
@@ -159,7 +158,9 @@ def notebooks():
             folder_notebooks, "02_model", "baseline_deep_dive.ipynb"
         ),
         "evaluation": os.path.join(folder_notebooks, "03_evaluate", "evaluation.ipynb"),
-        "fastai": os.path.join(folder_notebooks, "00_quick_start", "fastai_recommendation.ipynb")
+        "fastai": os.path.join(folder_notebooks, "00_quick_start", "fastai_recommendation.ipynb"),
+        "xdeepfm_quickstart": os.path.join(folder_notebooks, "00_quick_start", "xdeepfm.ipynb"),
+        "dkn_quickstart": os.path.join(folder_notebooks, "00_quick_start", "dkn.ipynb"),
     }
     return paths
 
