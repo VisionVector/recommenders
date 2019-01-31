38075b49dbba2946cd2fc4f174b859f60d000944
Jianxun Lian <jialia@microsoft.com>
2019-01-31 10:09:38 +0800

    correct typos for notebook xdeepfm

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@
 import calendar
 import datetime
 import os
-import numpy as np
 import pandas as pd
 import pytest
 from sklearn.model_selection import train_test_split
@@ -12,7 +11,7 @@ from tests.notebooks_common import path_notebooks
 
 try:
     from pyspark.sql import SparkSession
-except ImportError:
+except ModuleNotFoundError:
     pass  # so the environment without spark doesn't break
 
 
@@ -143,13 +142,9 @@ def notebooks():
         "sar_single_node": os.path.join(
             folder_notebooks, "00_quick_start", "sar_single_node_movielens.ipynb"
         ),
-        "ncf": os.path.join(
-            folder_notebooks, "00_quick_start", "ncf_movielens.ipynb"
-        ),
         "als_pyspark": os.path.join(
             folder_notebooks, "00_quick_start", "als_pyspark_movielens.ipynb"
         ),
-        "fastai": os.path.join(folder_notebooks, "00_quick_start", "fastai_recommendation.ipynb"),    
         "data_split": os.path.join(
             folder_notebooks, "01_prepare_data", "data_split.ipynb"
         ),
@@ -162,10 +157,10 @@ def notebooks():
         "baseline_deep_dive": os.path.join(
             folder_notebooks, "02_model", "baseline_deep_dive.ipynb"
         ),
-        "ncf_deep_dive": os.path.join(
-            folder_notebooks, "02_model", "ncf_deep_dive.ipynb"
-        ),
-            "evaluation": os.path.join(folder_notebooks, "03_evaluate", "evaluation.ipynb"),
-        }
+        "evaluation": os.path.join(folder_notebooks, "03_evaluate", "evaluation.ipynb"),
+        "fastai": os.path.join(folder_notebooks, "00_quick_start", "fastai_recommendation.ipynb"),
+        "xdeepfm_quickstart": os.path.join(folder_notebooks, "00_quick_start", "xdeepfm.ipynb"),
+        "dkn_quickstart": os.path.join(folder_notebooks, "00_quick_start", "dkn.ipynb"),
+    }
     return paths
 
