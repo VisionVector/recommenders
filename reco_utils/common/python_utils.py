503587f225f31626624839d72a3f81b221aabe0d
VisionVector <markofejzo272@gmail.com>
2019-02-01 22:06:13 +0000

    trying to solve OOM

--- reco_utils/common/python_utils.py
+++ reco_utils/common/python_utils.py
@@ -2,50 +2,29 @@ import numpy as np
 from scipy.sparse import coo_matrix
 
 
-def exponential_decay(value, max_val, half_life):
-    """Compute decay factor for a given value based on an exponential decay
-    Args:
-        value (numeric): value to calculate decay factor
-        max_val (numeric): value at which decay factor will be 1
-        half_life (numeric): value at which decay factor will be 0.5
-    Returns:
-        (float): decay factor
-    """
-
-    return np.exp(-np.log(2) * (max_val - value) / half_life)
-
-
 def jaccard(cooccurrence):
-    """Helper method to calculate the Jaccard similarity of a matrix of co-occurrences
+    """Helper method to calculate the Jaccard similarity of a matrix of cooccurrences
     Args:
-        cooccurrence (np.matrix): the symmetric matrix of co-occurrences of items
+        cooccurrence (scipy.sparse.csc_matrix): the symmetric matrix of cooccurrences of items
     Returns:
-        (np.matrix): The matrix of Jaccard similarities between any two items
+        scipy.sparse.coo_matrix: The matrix of Jaccard similarities between any two items
     """
-
-    diag = cooccurrence.diagonal()
-    diag_rows = np.expand_dims(diag, axis=0)
-    diag_cols = np.expand_dims(diag, axis=1)
-
-    with np.errstate(invalid='ignore', divide='ignore'):
-        result = cooccurrence / (diag_rows + diag_cols - cooccurrence)
-
-    return result
+    coo = cooccurrence.tocoo()
+    denom = coo.diagonal()[coo.row] + coo.diagonal()[coo.col] - coo.data
+    return coo_matrix((np.divide(coo.data, denom, out=np.zeros_like(coo.data), where=(denom != 0.0)),
+                        (coo.row, coo.col)),
+                      shape=coo.shape).tocsc()
 
 
 def lift(cooccurrence):
-    """Helper method to calculate the Lift of a matrix of co-occurrences
+    """Helper method to calculate the Lift of a matrix of cooccurrences
     Args:
-        cooccurrence ((np.matrix): the symmetric matrix of co-occurrences of items
+        cooccurrence (scipy.sparse.csc_matrix): the symmetric matrix of cooccurrences of items
     Returns:
-        (np.matrix): The matrix of Lifts between any two items
+        scipy.sparse.coo_matrix: The matrix of Lifts between any two items
     """
-
-    diag = cooccurrence.diagonal()
-    diag_rows = np.expand_dims(diag, axis=0)
-    diag_cols = np.expand_dims(diag, axis=1)
-
-    with np.errstate(invalid='ignore', divide='ignore'):
-        result = cooccurrence / (diag_rows * diag_cols)
-
-    return result
+    coo = cooccurrence.tocoo()
+    denom = coo.diagonal()[coo.row] * coo.diagonal()[coo.col]
+    return coo_matrix((np.divide(coo.data, denom, out=np.zeros_like(coo.data), where=(denom != 0.0)),
+                        (coo.row, coo.col)),
+                       shape=coo.shape).tocsc()
