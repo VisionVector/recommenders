694979258a4c9ddd1a73c10203eb0b9a5f6477a3
Jun Ki Min <42475935+loomlike@users.noreply.github.com>
2019-03-05 10:07:28 -0500

    Change a way to display DataFrames

--- reco_utils/dataset/pandas_df_utils.py
+++ reco_utils/dataset/pandas_df_utils.py
@@ -5,8 +5,7 @@ import pandas as pd
 
 from reco_utils.common.constants import (
     DEFAULT_USER_COL,
-    DEFAULT_ITEM_COL,
-    DEFAULT_RATING_COL,
+    DEFAULT_ITEM_COL
 )
 
 
@@ -16,7 +15,7 @@ def user_item_pairs(
     user_col=DEFAULT_USER_COL,
     item_col=DEFAULT_ITEM_COL,
     user_item_filter_df=None,
-    shuffle=True,
+    shuffle=True
 ):
     """Get all pairs of users and items data.
 
@@ -33,22 +32,17 @@ def user_item_pairs(
     """
 
     # Get all user-item pairs
-    user_df["key"] = 1
-    item_df["key"] = 1
-    users_items = user_df.merge(item_df, on="key")
+    user_df['key'] = 1
+    item_df['key'] = 1
+    users_items = user_df.merge(item_df, on='key')
 
-    user_df.drop("key", axis=1, inplace=True)
-    item_df.drop("key", axis=1, inplace=True)
-    users_items.drop("key", axis=1, inplace=True)
+    user_df.drop('key', axis=1, inplace=True)
+    item_df.drop('key', axis=1, inplace=True)
+    users_items.drop('key', axis=1, inplace=True)
 
     # Filter
     if user_item_filter_df is not None:
-        user_item_col = [user_col, item_col]
-        users_items = users_items.loc[
-            ~users_items.set_index(user_item_col).index.isin(
-                user_item_filter_df.set_index(user_item_col).index
-            )
-        ]
+        users_items = filter_by(users_items, user_item_filter_df, [user_col, item_col])
 
     if shuffle:
         users_items = users_items.sample(frac=1).reset_index(drop=True)
@@ -70,7 +64,5 @@ def filter_by(df, filter_by_df, filter_by_cols):
     """
 
     return df.loc[
-        ~df.set_index(filter_by_cols).index.isin(
-            filter_by_df.set_index(filter_by_cols).index
-        )
+        ~df.set_index(filter_by_cols).index.isin(filter_by_df.set_index(filter_by_cols).index)
     ]
