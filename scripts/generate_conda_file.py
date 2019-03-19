565240c2e2e6d08ac9b944a5e3094ecee39bf4d3
Jun Ki Min <42475935+loomlike@users.noreply.github.com>
2019-03-19 16:30:31 -0400

    Update SETUP.md for more databricks info

--- scripts/generate_conda_file.py
+++ scripts/generate_conda_file.py
@@ -60,7 +60,7 @@ CONDA_PYSPARK = {"pyarrow": "pyarrow>=0.8.0", "pyspark": "pyspark==2.3.1"}
 CONDA_GPU = {"numba": "numba>=0.38.1", "pytorch": "pytorch>=1.0.0", "tensorflow": "tensorflow-gpu==1.12.0"}
 
 PIP_BASE = {
-    "azureml-sdk[notebooks,contrib]": "azureml-sdk[notebooks,contrib]==1.0.10",
+    "azureml-sdk[notebooks,tensorboard,contrib]": "azureml-sdk[notebooks,tensorboard,contrib]==1.0.18",
     "azure-storage": "azure-storage>=0.36.0",
     "black": "black>=18.6b4",
     "dataclasses": "dataclasses>=0.6",
@@ -72,7 +72,6 @@ PIP_BASE = {
     "pydocumentdb": "pydocumentdb>=2.3.3",
     "category_encoders": "category_encoders>=1.3.0",
     "fastai": "fastai==1.0.46",
-    "nni": "nni==0.5.2.1"
 }
 
 PIP_PYSPARK = {}
