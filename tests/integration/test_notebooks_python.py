f0156c81590fad71dcdb9333e054853cafd8b391
VisionVector <markofejzo272@gmail.com>
2019-03-28 08:29:39 +0000

    Merge pull request #688 from jingyanwangms/jingywa/freeze-papermill-version

--- tests/integration/test_notebooks_python.py
+++ tests/integration/test_notebooks_python.py
@@ -7,6 +7,7 @@ from tests.notebooks_common import OUTPUT_NOTEBOOK, KERNEL_NAME
 
 
 TOL = 0.05
+ABS_TOL = 0.05
 
 
 @pytest.mark.integration
@@ -23,14 +24,14 @@ TOL = 0.05
             },
         ),
         (
-            "10m", 
+            "10m",
             {
-                "map": 0.101402, 
-                "ndcg": 0.321073, 
-                "precision": 0.275766, 
-                "recall": 0.156483
-            }
-        ) 
+                "map": 0.101402,
+                "ndcg": 0.321073,
+                "precision": 0.275766,
+                "recall": 0.156483,
+            },
+        ),
     ],
 )
 def test_sar_single_node_integration(notebooks, size, expected_values):
@@ -44,7 +45,7 @@ def test_sar_single_node_integration(notebooks, size, expected_values):
     results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
 
     for key, value in expected_values.items():
-        assert results[key] == pytest.approx(value, rel=TOL)
+        assert results[key] == pytest.approx(value, rel=TOL, abs=ABS_TOL)
 
 
 @pytest.mark.integration
@@ -75,7 +76,7 @@ def test_baseline_deep_dive_integration(notebooks, size, expected_values):
     results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
 
     for key, value in expected_values.items():
-        assert results[key] == pytest.approx(value, rel=TOL)
+        assert results[key] == pytest.approx(value, rel=TOL, abs=ABS_TOL)
 
 
 @pytest.mark.integration
@@ -109,7 +110,7 @@ def test_surprise_svd_integration(notebooks, size, expected_values):
     results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
 
     for key, value in expected_values.items():
-        assert results[key] == pytest.approx(value, rel=TOL)
+        assert results[key] == pytest.approx(value, rel=TOL, abs=ABS_TOL)
 
 
 @pytest.mark.integration
@@ -119,14 +120,14 @@ def test_surprise_svd_integration(notebooks, size, expected_values):
         (
             "1m",
             dict(
-                rmse=0.9555,
-                mae=0.68493,
-                rsquared=0.26547,
-                exp_var=0.26615,
-                map=0.50635,
-                ndcg=0.99966,
-                precision=0.92684,
-                recall=0.50635,
+                rmse=0.958504,
+                mae=0.686706,
+                rsquared=0.26606,
+                exp_var=0.266197,
+                map=0.186626,
+                ndcg=0.681827,
+                precision=0.5748,
+                recall=0.186626,
             ),
         )
     ],
@@ -142,4 +143,4 @@ def test_vw_deep_dive_integration(notebooks, size, expected_values):
     results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
 
     for key, value in expected_values.items():
-        assert results[key] == pytest.approx(value, rel=TOL)
+        assert results[key] == pytest.approx(value, rel=TOL, abs=ABS_TOL)
