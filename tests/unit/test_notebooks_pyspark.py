09fa352b23398d9b671f41ff3a276af96f506c5d
VisionVector <markofejzo272@gmail.com>
2019-04-03 15:13:11 +0100

    refactored and improved speed :rocket:

--- tests/unit/test_notebooks_pyspark.py
+++ tests/unit/test_notebooks_pyspark.py
@@ -33,3 +33,35 @@ def test_evaluation_runs(notebooks):
     notebook_path = notebooks["evaluation"]
     pm.execute_notebook(notebook_path, OUTPUT_NOTEBOOK, kernel_name=KERNEL_NAME)
 
+
+@pytest.mark.notebooks
+@pytest.mark.spark
+def test_spark_tuning(notebooks):
+    notebook_path = notebooks["spark_tuning"]
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        kernel_name=KERNEL_NAME,
+        parameters=dict(
+            NUMBER_CORES="*",
+            NUMBER_ITERATIONS=3,
+            RANK=[5, 5],
+            REG=[0.1, 0.01]
+        )
+    )
+
+
+@pytest.mark.notebooks
+@pytest.mark.spark
+def test_mmlspark_lightgbm_criteo_runs(notebooks):
+    notebook_path = notebooks["mmlspark_lightgbm_criteo"]
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        kernel_name=KERNEL_NAME,
+        parameters=dict(
+            DATA_SIZE="sample",
+            NUM_ITERATIONS=10,
+            EARLY_STOPPING_ROUND=2,
+        )
+    )
