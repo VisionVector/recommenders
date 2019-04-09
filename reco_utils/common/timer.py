e074ab90b1826b09d7fc08881190fbea0767fcdf
VisionVector <markofejzo272@gmail.com>
2019-04-09 18:30:25 +0100

    Merge pull request #723 from tandav/master

--- reco_utils/common/timer.py
+++ reco_utils/common/timer.py
@@ -38,7 +38,7 @@ class Timer(object):
         self.stop()
 
     def __str__(self):
-        return "{:0.4f}".format(self.interval)
+        return str(timedelta(seconds=self._interval))
 
     def start(self):
         """Start the timer."""
@@ -62,4 +62,3 @@ class Timer(object):
             raise ValueError("Timer has not been stopped, please use stop().")
         else:
             return self._interval
-
