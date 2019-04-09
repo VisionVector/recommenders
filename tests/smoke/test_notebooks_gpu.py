d181fdc3d1e90f017d32de9fd9a0eeeecbce6cdb
Alexander Rodionov <alx1dr@gmail.com>
2019-04-09 15:07:45 +0300

    fix specific col_item string name

--- tests/smoke/test_notebooks_gpu.py
+++ tests/smoke/test_notebooks_gpu.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+import shutil
 import papermill as pm
 import pytest
 from reco_utils.common.gpu_utils import get_number_gpus
@@ -96,7 +97,7 @@ def test_notebook_xdeepfm(notebooks):
             EPOCHS_FOR_SYNTHETIC_RUN=20,
             EPOCHS_FOR_CRITEO_RUN=1,
             BATCH_SIZE_SYNTHETIC=128,
-            BATCH_SIZE_CRITEO=512,
+            BATCH_SIZE_CRITEO=2048,
         ),
     )
     results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
@@ -127,15 +128,16 @@ def test_notebook_dkn(notebooks):
 
 @pytest.mark.smoke
 @pytest.mark.gpu
-def test_wide_deep(notebooks, tmp):
+def test_wide_deep(notebooks):
     notebook_path = notebooks["wide_deep"]
 
+    MODEL_DIR = "model_checkpoints"
     params = {
         "MOVIELENS_DATA_SIZE": "100k",
         "EPOCHS": 1,
         "EVALUATE_WHILE_TRAINING": False,
-        "MODEL_DIR": tmp,
-        "EXPORT_DIR_BASE": tmp,
+        "MODEL_DIR": MODEL_DIR,
+        "EXPORT_DIR_BASE": MODEL_DIR,
         "RATING_METRICS": ["rmse", "mae"],
         "RANKING_METRICS": ["ndcg_at_k", "precision_at_k"],
     }
@@ -152,3 +154,5 @@ def test_wide_deep(notebooks, tmp):
     assert results["mae"] < 2.0
     assert results["ndcg_at_k"] > 0.0
     assert results["precision_at_k"] > 0.0
+
+    shutil.rmtree(MODEL_DIR, ignore_errors=True)
