2cc3525b9ea6a22b4feb97584652e037d6a960e5
Andreas Argyriou <anargyri@microsoft.com>
2019-04-17 18:36:40 +0000

    NNI: add data to test

--- tests/unit/test_nni_utils.py
+++ tests/unit/test_nni_utils.py
@@ -37,89 +37,88 @@ def mocked_trials_get(url, content):
     return MockResponse(content)
     
 
-def mock_exception():
-    raise Exception()
-
-
+@patch('requests.get', side_effect=lambda url: mocked_status_get(url, {'status': 'some_status'}))
 def test_get_experiment_status():
-    content = {'status': 'some_status'}
-    with patch('requests.get', side_effect=lambda url: mocked_status_get(url, content)):
-        assert 'some_status' == get_experiment_status(NNI_STATUS_URL)
+    assert 'some_status' == get_experiment_status(NNI_STATUS_URL)
 
 
+@patch('requests.get', side_effect=lambda url: mocked_status_get(url, {'status': 'DONE'}))
 def test_check_experiment_status_done():
-    content = {'status': 'DONE'}
-    with patch('requests.get', side_effect=lambda url: mocked_status_get(url, content)):
-        check_experiment_status(wait=0.1, max_retries=1)
+    check_experiment_status(1)
 
 
+@patch('requests.get', side_effect=lambda url: mocked_status_get(url, {'status': 'TUNER_NO_MORE_TRIAL'}))
 def test_check_experiment_status_tuner_no_more_trial():
-    content = {'status': 'TUNER_NO_MORE_TRIAL'}
-    with patch('requests.get', side_effect=lambda url: mocked_status_get(url, content)):
-        check_experiment_status(wait=0.1, max_retries=1)
+    check_experiment_status(1)
 
 
+@patch('requests.get', side_effect=lambda url: mocked_status_get(url, {'status': 'RUNNING'}))
 def test_check_experiment_status_running():
-    content = {'status': 'RUNNING'}
     with pytest.raises(TimeoutError) as excinfo:
-        with patch('requests.get', side_effect=lambda url: mocked_status_get(url, content)):
-            check_experiment_status(wait=0.1, max_retries=1)
+        check_experiment_status(1)
     assert "check_experiment_status() timed out" == str(excinfo.value)
 
 
+@patch('requests.get', side_effect=lambda url: mocked_status_get(url, {'status': 'NO_MORE_TRIAL'}))
 def test_check_experiment_status_no_more_trial():
-    content = {'status': 'NO_MORE_TRIAL'}
     with pytest.raises(TimeoutError) as excinfo:
-        with patch('requests.get', side_effect=lambda url: mocked_status_get(url, content)):
-            check_experiment_status(wait=0.1, max_retries=1)
+        check_experiment_status(1)
     assert "check_experiment_status() timed out" == str(excinfo.value)
 
 
+@patch('requests.get', side_effect=lambda url: mocked_status_get(url, {'status': 'some_failed_status'}))
 def test_check_experiment_status_failed():
-    content = {'status': 'some_failed_status'}
     with pytest.raises(RuntimeError) as excinfo:
-        with patch('requests.get', side_effect=lambda url: mocked_status_get(url, content)):
-            check_experiment_status(wait=0.1, max_retries=1)
+        check_experiment_status(1)
     assert "NNI experiment failed to complete with status some_failed_status" == str(excinfo.value)
 
 
+@patch('requests.get', side_effect=lambda url: mocked_status_get(url, {'status': 'some_status'}))
 def test_check_stopped_timeout():
-    content = {'status': 'some_status'}
     with pytest.raises(TimeoutError) as excinfo:
-        with patch('requests.get', side_effect=lambda url: mocked_status_get(url, content)):
-            check_stopped(wait=.1, max_retries=1)
+        check_stopped(wait=1, max_retries=1)
     assert "check_stopped() timed out" == str(excinfo.value)
 
 
 def test_check_stopped():
-    with patch('requests.get', side_effect=mock_exception):
-        check_stopped(wait=.1, max_retries=1)
+    check_stopped(wait=1, max_retries=1)
 
 
+@patch('requests.get',
+       side_effect=lambda url: mocked_status_get(url, [{'finalMetricData': None}, {'finalMetricData': None}]))
 def test_check_metrics_written():
-    content = [{'finalMetricData': None}, {'finalMetricData': None}]
-    with patch('requests.get', side_effect=lambda url: mocked_trials_get(url, content)):
-        check_metrics_written(wait=.1, max_retries=1)
+    check_metrics_written(wait=1, max_retries=1)
 
 
+@patch('requests.get', side_effect=lambda url: mocked_status_get(url, [{'logPath': '/p'}, {'logPath': '/q'}]))
 def test_check_metrics_written_timeout():
-    content = [{'logPath': '/p'}, {'logPath': '/q'}]
     with pytest.raises(TimeoutError) as excinfo:
-        with patch('requests.get', side_effect=lambda url: mocked_trials_get(url, content)):
-            check_metrics_written(wait=.1, max_retries=1)
+        check_metrics_written(wait=1, max_retries=1)
     assert "check_metrics_written() timed out" == str(excinfo.value)
 
 
 def test_get_trials():
-    with TemporaryDirectory() as tmp_dir:
+    with TemporaryDirectory() as tmp_dir1, TemporaryDirectory() as tmp_dir2:
         mock_trials = [
-            {'finalMetricData': [{'data': '{"default": 1}'}], 'logPath': 'file://localhost:{}'.format(tmp_dir)},
-            {'finalMetricData': [{'data': '{"default": 2}'}], 'logPath': 'file://localhost:{}'.format(tmp_dir)}
+            {'finalMetricData': [{'data': '{"rmse":0.8,"default":0.3}'}], 
+            'logPath': 'file://localhost:{}'.format(tmp_dir1)},
+            {'finalMetricData': [{'data': '{"rmse":0.9,"default":0.2}'}], 
+            'logPath': 'file://localhost:{}'.format(tmp_dir2)},
         ]
-        with open(os.path.join(tmp_dir, 'metrics.json'), 'w') as f:
-            json.dump(dict(a=1), f)
-        with open(os.path.join(tmp_dir, 'parameter.cfg'), 'w') as f:
-            json.dump(dict(b=2), f)
+        metrics1 = {"rmse": 0.8, precision_at_k": 0.3}
+        with open(os.path.join(tmp_dir1, 'metrics.json'), 'w') as f:
+            json.dump(metrics1, f)
+        params1 = {"parameter_id": 1, "parameter_source": "algorithm", 
+        "parameters": {"n_factors": 100, "reg": 0.1}}
+        with open(os.path.join(tmp_dir1, 'parameter.cfg'), 'w') as f:
+            json.dump(params1, f)
+        metrics2 = {"rmse": 0.9, precision_at_k": 0.2}
+        with open(os.path.join(tmp_dir2, 'metrics.json'), 'w') as f:
+            json.dump(metrics2, f)
+        params2 = {"parameter_id": 2, "parameter_source": "algorithm", 
+        "parameters": {"n_factors": 50, "reg": 0.02}}
+        with open(os.path.join(tmp_dir2, 'parameter.cfg'), 'w') as f:
+            json.dump(params2, f)
 
         with patch('requests.get', side_effect=lambda url: mocked_trials_get(url, mock_trials)):
             trials, best_metrics, best_params, best_trial_path = get_trials(optimize_mode='maximize')
