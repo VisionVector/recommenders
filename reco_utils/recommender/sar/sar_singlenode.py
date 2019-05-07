880cd2b8930350d7552df249eff70f2a3c5592b2
VisionVector <markofejzo272@gmail.com>
2019-05-07 19:50:13 +0100

    Merge pull request #776 from microsoft/roalexan/windowstestsstatus

--- reco_utils/recommender/sar/sar_singlenode.py
+++ reco_utils/recommender/sar/sar_singlenode.py
@@ -1,7 +1,14 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+"""
+Reference implementation of SAR in python/numpy/pandas.
 
+This is not meant to be particularly performant or scalable, just
+a simple and readable implementation.
+"""
+
+from enum import Enum
 import numpy as np
 import pandas as pd
 import logging
@@ -16,9 +23,7 @@ from reco_utils.common.python_utils import (
 from reco_utils.common import constants
 
 
-COOCUR = "cooccurrence"
-JACCARD = "jaccard"
-LIFT = "lift"
+SIMILARITY_TYPE = Enum("SIMILARITY_TYPE", "cooccurrence jaccard lift")
 
 logger = logging.getLogger()
 
@@ -28,12 +33,13 @@ class SARSingleNode:
 
     def __init__(
         self,
+        remove_seen=True,
         col_user=constants.DEFAULT_USER_COL,
         col_item=constants.DEFAULT_ITEM_COL,
         col_rating=constants.DEFAULT_RATING_COL,
         col_timestamp=constants.DEFAULT_TIMESTAMP_COL,
         col_prediction=constants.DEFAULT_PREDICTION_COL,
-        similarity_type=JACCARD,
+        similarity_type="jaccard",
         time_decay_coefficient=30,
         time_now=None,
         timedecay_formula=False,
@@ -58,16 +64,22 @@ class SARSingleNode:
         self.col_user = col_user
         self.col_timestamp = col_timestamp
         self.col_prediction = col_prediction
-        
-        if similarity_type not in [COOCUR, JACCARD, LIFT]:
-            raise ValueError('Similarity type must be one of ["cooccurrence" | "jaccard" | "lift"]')
-        self.similarity_type = similarity_type
+
+        self.remove_seen = remove_seen
+
+        try:
+            self.similarity_type = SIMILARITY_TYPE[similarity_type]
+        except KeyError:
+            raise KeyError(
+                'similarity type must be one of ["cooccurrence" | "jaccard" | "lift"]'
+            )
         self.time_decay_half_life = (
             time_decay_coefficient * 24 * 60 * 60
         )  # convert to seconds
         self.time_decay_flag = timedecay_formula
         self.time_now = time_now
         self.threshold = threshold
+
         self.user_affinity = None
         self.item_similarity = None
         self.item_frequencies = None
@@ -100,7 +112,6 @@ class SARSingleNode:
         indices in a sparse matrix, and the events as the data. Here, we're treating
         the ratings as the event weights.  We convert between different sparse-matrix
         formats to de-duplicate user-item pairs, otherwise they will get added up.
-        
         Args:
             df (pd.DataFrame): Indexed df of users and items.
             n_users (int): Number of users.
@@ -229,15 +240,15 @@ class SARSingleNode:
         self.item_frequencies = item_cooccurrence.diagonal()
 
         logger.info("Calculating item similarity")
-        if self.similarity_type is COOCUR:
+        if self.similarity_type is SIMILARITY_TYPE.cooccurrence:
             logger.info("Using co-occurrence based similarity")
             self.item_similarity = item_cooccurrence
-        elif self.similarity_type is JACCARD:
+        elif self.similarity_type is SIMILARITY_TYPE.jaccard:
             logger.info("Using jaccard based similarity")
             self.item_similarity = jaccard(item_cooccurrence).astype(
                 df[self.col_rating].dtype
             )
-        elif self.similarity_type is LIFT:
+        elif self.similarity_type is SIMILARITY_TYPE.lift:
             logger.info("Using lift based similarity")
             self.item_similarity = lift(item_cooccurrence).astype(
                 df[self.col_rating].dtype
