82835653579a067cead41e8d83ae08b41921e245
VisionVector <markofejzo272@gmail.com>
2020-04-27 12:37:26 +0100

    refact

--- examples/06_benchmarks/benchmark_utils.py
+++ examples/06_benchmarks/benchmark_utils.py
@@ -1,25 +1,16 @@
-# Copyright (c) Recommenders contributors.
-# Licensed under the MIT License.
+import sys
 
-import os
-import numpy as np
+sys.path.append("../")
 import pandas as pd
-from tempfile import TemporaryDirectory
+import numpy as np
+from pyspark.ml.recommendation import ALS
+from pyspark.sql.types import StructType, StructField
+from pyspark.sql.types import StringType, FloatType, IntegerType, LongType
+from fastai.collab import collab_learner, CollabDataBunch
+import surprise
 import cornac
 
-try:
-    from pyspark.ml.recommendation import ALS
-    from pyspark.sql.types import StructType, StructField
-    from pyspark.sql.types import FloatType, IntegerType, LongType
-except ImportError:
-    pass  # skip this import if we are not in a Spark environment
-try:
-    import surprise  # Put SVD surprise back in core deps when #2224 is fixed
-except:
-    pass
-
-from recommenders.utils.timer import Timer
-from recommenders.utils.constants import (
+from reco_utils.common.constants import (
     COL_DICT,
     DEFAULT_K,
     DEFAULT_USER_COL,
@@ -29,51 +20,35 @@ from recommenders.utils.constants import (
     DEFAULT_TIMESTAMP_COL,
     SEED,
 )
-from recommenders.models.sar import SAR
-from recommenders.models.cornac.bpr import BPR
-from recommenders.models.cornac.cornac_utils import predict_ranking
-from recommenders.models.surprise.surprise_utils import (
+from reco_utils.common.timer import Timer
+from reco_utils.common.spark_utils import start_or_get_spark
+from reco_utils.recommender.sar.sar_singlenode import SARSingleNode
+from reco_utils.recommender.ncf.ncf_singlenode import NCF
+from reco_utils.recommender.ncf.dataset import Dataset as NCFDataset
+from reco_utils.recommender.surprise.surprise_utils import (
     predict,
     compute_ranking_predictions,
 )
-from recommenders.evaluation.python_evaluation import (
-    map,
+from reco_utils.recommender.fastai.fastai_utils import (
+    cartesian_product,
+    score,
+    hide_fastai_progress_bar,
+)
+from reco_utils.recommender.cornac.cornac_utils import predict_ranking
+from reco_utils.evaluation.spark_evaluation import (
+    SparkRatingEvaluation,
+    SparkRankingEvaluation,
+)
+from reco_utils.evaluation.python_evaluation import (
+    map_at_k,
     ndcg_at_k,
     precision_at_k,
     recall_at_k,
 )
-from recommenders.evaluation.python_evaluation import rmse, mae, rsquared, exp_var
-
-try:
-    from recommenders.utils.spark_utils import start_or_get_spark
-    from recommenders.evaluation.spark_evaluation import (
-        SparkRatingEvaluation,
-        SparkRankingEvaluation,
-    )
-except (ImportError, NameError):
-    pass  # skip this import if we are not in a Spark environment
-
-try:
-    from recommenders.models.deeprec.deeprec_utils import prepare_hparams
-    from recommenders.models.deeprec.models.graphrec.lightgcn import LightGCN
-    from recommenders.models.deeprec.DataModel.ImplicitCF import ImplicitCF
-    from recommenders.models.ncf.ncf_singlenode import NCF
-    from recommenders.models.ncf.dataset import Dataset as NCFDataset
-    from recommenders.models.embdotbias.model import EmbeddingDotBias
-    from recommenders.models.embdotbias.data_loader import RecoDataLoader
-    from recommenders.models.embdotbias.training_utils import Trainer
-    from recommenders.models.embdotbias.utils import cartesian_product, score
-
-except ImportError:
-    pass  # skip this import if we are not in a GPU environment
-
-# Helpers
-tmp_dir = TemporaryDirectory()
-TRAIN_FILE = os.path.join(tmp_dir.name, "df_train.csv")
-TEST_FILE = os.path.join(tmp_dir.name, "df_test.csv")
+from reco_utils.evaluation.python_evaluation import rmse, mae, rsquared, exp_var
 
 
-def prepare_training_als(train, test):
+def prepare_training_als(train):
     schema = StructType(
         (
             StructField(DEFAULT_USER_COL, IntegerType()),
@@ -83,7 +58,7 @@ def prepare_training_als(train, test):
         )
     )
     spark = start_or_get_spark()
-    return spark.createDataFrame(train, schema).cache()
+    return spark.createDataFrame(train, schema)
 
 
 def train_als(params, data):
@@ -103,10 +78,7 @@ def prepare_metrics_als(train, test):
         )
     )
     spark = start_or_get_spark()
-    return (
-        spark.createDataFrame(train, schema).cache(),
-        spark.createDataFrame(test, schema).cache(),
-    )
+    return prepare_training_als(train), spark.createDataFrame(test, schema)
 
 
 def predict_als(model, test):
@@ -115,7 +87,7 @@ def predict_als(model, test):
     return preds, t
 
 
-def recommend_k_als(model, test, train, top_k=DEFAULT_K, remove_seen=True):
+def recommend_k_als(model, test, train):
     with Timer() as t:
         # Get the cross join of all user-item pairs and score them.
         users = train.select(DEFAULT_USER_COL).distinct()
@@ -123,7 +95,7 @@ def recommend_k_als(model, test, train, top_k=DEFAULT_K, remove_seen=True):
         user_item = users.crossJoin(items)
         dfs_pred = model.transform(user_item)
 
-        # Remove seen items
+        # Remove seen items.
         dfs_pred_exclude_train = dfs_pred.alias("pred").join(
             train.alias("train"),
             (dfs_pred[DEFAULT_USER_COL] == train[DEFAULT_USER_COL])
@@ -140,7 +112,7 @@ def recommend_k_als(model, test, train, top_k=DEFAULT_K, remove_seen=True):
     return topk_scores, t
 
 
-def prepare_training_svd(train, test):
+def prepare_training_svd(train):
     reader = surprise.Reader("ml-100k", rating_scale=(1, 5))
     return surprise.Dataset.load_from_df(
         train.drop(DEFAULT_TIMESTAMP_COL, axis=1), reader=reader
@@ -166,7 +138,7 @@ def predict_svd(model, test):
     return preds, t
 
 
-def recommend_k_svd(model, test, train, top_k=DEFAULT_K, remove_seen=True):
+def recommend_k_svd(model, test, train):
     with Timer() as t:
         topk_scores = compute_ranking_predictions(
             model,
@@ -174,51 +146,42 @@ def recommend_k_svd(model, test, train, top_k=DEFAULT_K, remove_seen=True):
             usercol=DEFAULT_USER_COL,
             itemcol=DEFAULT_ITEM_COL,
             predcol=DEFAULT_PREDICTION_COL,
-            remove_seen=remove_seen,
+            remove_seen=True,
         )
     return topk_scores, t
 
 
-def prepare_training_embdotbias(train, test):
-    train_df = train.copy()
-    train_df[DEFAULT_USER_COL] = train_df[DEFAULT_USER_COL].astype("str")
-    train_df[DEFAULT_ITEM_COL] = train_df[DEFAULT_ITEM_COL].astype("str")
-    data = RecoDataLoader.from_df(
-        train_df,
+def prepare_training_fastai(train):
+    data = train.copy()
+    data[DEFAULT_USER_COL] = data[DEFAULT_USER_COL].astype("str")
+    data[DEFAULT_ITEM_COL] = data[DEFAULT_ITEM_COL].astype("str")
+    data = CollabDataBunch.from_df(
+        data,
         user_name=DEFAULT_USER_COL,
         item_name=DEFAULT_ITEM_COL,
         rating_name=DEFAULT_RATING_COL,
-        valid_pct=0.1,
+        valid_pct=0,
     )
     return data
 
 
-def train_embdotbias(params, data):
-    model = EmbeddingDotBias.from_classes(
-        n_factors=params["n_factors"],
-        classes=data.classes,
-        user=DEFAULT_USER_COL,
-        item=DEFAULT_ITEM_COL,
-        y_range=params.get("y_range", [0, 5.5]),
+def train_fastai(params, data):
+    model = collab_learner(
+        data, n_factors=params["n_factors"], y_range=params["y_range"], wd=params["wd"]
     )
-
     with Timer() as t:
-        trainer = Trainer(model=model)
-        trainer.fit(data.train, data.valid, params["epochs"])
+        model.fit_one_cycle(cyc_len=params["epochs"], max_lr=params["max_lr"])
     return model, t
 
 
-def prepare_metrics_embdotbias(train, test):
-    train_df = train.copy()
-    train_df[DEFAULT_USER_COL] = train_df[DEFAULT_USER_COL].astype("str")
-    train_df[DEFAULT_ITEM_COL] = train_df[DEFAULT_ITEM_COL].astype("str")
-    test_df = test.copy()
-    test_df[DEFAULT_USER_COL] = test_df[DEFAULT_USER_COL].astype("str")
-    test_df[DEFAULT_ITEM_COL] = test_df[DEFAULT_ITEM_COL].astype("str")
-    return train_df, test_df
+def prepare_metrics_fastai(train, test):
+    data = test.copy()
+    data[DEFAULT_USER_COL] = data[DEFAULT_USER_COL].astype("str")
+    data[DEFAULT_ITEM_COL] = data[DEFAULT_ITEM_COL].astype("str")
+    return train, data
 
 
-def predict_embdotbias(model, test):
+def predict_fastai(model, test):
     with Timer() as t:
         preds = score(
             model,
@@ -230,53 +193,44 @@ def predict_embdotbias(model, test):
     return preds, t
 
 
-def recommend_k_embdotbias(model, test, train, top_k=DEFAULT_K, remove_seen=True):
-    # Get all users/items known to the model
-    total_users = model.classes[DEFAULT_USER_COL][1:]
-    total_items = model.classes[DEFAULT_ITEM_COL][1:]
-    test_users = test[DEFAULT_USER_COL].unique()
-    test_users = np.intersect1d(test_users, total_users)
-    users_items = cartesian_product(np.array(test_users), np.array(total_items))
-    users_items = pd.DataFrame(
-        users_items, columns=[DEFAULT_USER_COL, DEFAULT_ITEM_COL]
-    )
-    if remove_seen:
-        # Remove seen items
+def recommend_k_fastai(model, test, train):
+    with Timer() as t:
+        total_users, total_items = model.data.train_ds.x.classes.values()
+        total_items = total_items[1:]
+        total_users = total_users[1:]
+        test_users = test[DEFAULT_USER_COL].unique()
+        test_users = np.intersect1d(test_users, total_users)
+        users_items = cartesian_product(test_users, total_items)
+        users_items = pd.DataFrame(
+            users_items, columns=[DEFAULT_USER_COL, DEFAULT_ITEM_COL]
+        )
         training_removed = pd.merge(
             users_items,
             train.astype(str),
             on=[DEFAULT_USER_COL, DEFAULT_ITEM_COL],
             how="left",
         )
-        candidates = training_removed[training_removed[DEFAULT_RATING_COL].isna()][
-            [DEFAULT_USER_COL, DEFAULT_ITEM_COL]
-        ]
-    else:
-        candidates = users_items
-    with Timer() as t:
+        training_removed = training_removed[
+            training_removed[DEFAULT_RATING_COL].isna()
+        ][[DEFAULT_USER_COL, DEFAULT_ITEM_COL]]
         topk_scores = score(
             model,
-            test_df=candidates,
+            test_df=training_removed,
             user_col=DEFAULT_USER_COL,
             item_col=DEFAULT_ITEM_COL,
             prediction_col=DEFAULT_PREDICTION_COL,
-            top_k=top_k,
+            top_k=DEFAULT_K,
         )
     return topk_scores, t
 
 
-def prepare_training_ncf(df_train, df_test):
-    train = df_train.sort_values([DEFAULT_USER_COL], axis=0, ascending=[True])
-    test = df_test.sort_values([DEFAULT_USER_COL], axis=0, ascending=[True])
-    test = test[df_test[DEFAULT_USER_COL].isin(train[DEFAULT_USER_COL].unique())]
-    test = test[test[DEFAULT_ITEM_COL].isin(train[DEFAULT_ITEM_COL].unique())]
-    train.to_csv(TRAIN_FILE, index=False)
-    test.to_csv(TEST_FILE, index=False)
+def prepare_training_ncf(train):
     return NCFDataset(
-        train_file=TRAIN_FILE,
+        train=train,
         col_user=DEFAULT_USER_COL,
         col_item=DEFAULT_ITEM_COL,
         col_rating=DEFAULT_RATING_COL,
+        col_timestamp=DEFAULT_TIMESTAMP_COL,
         seed=SEED,
     )
 
@@ -288,7 +242,7 @@ def train_ncf(params, data):
     return model, t
 
 
-def recommend_k_ncf(model, test, train, top_k=DEFAULT_K, remove_seen=True):
+def recommend_k_ncf(model, test, train):
     with Timer() as t:
         users, items, preds = [], [], []
         item = list(train[DEFAULT_ITEM_COL].unique())
@@ -310,44 +264,23 @@ def recommend_k_ncf(model, test, train, top_k=DEFAULT_K, remove_seen=True):
         topk_scores = merged[merged[DEFAULT_RATING_COL].isnull()].drop(
             DEFAULT_RATING_COL, axis=1
         )
-    # Remove temp files
     return topk_scores, t
 
 
-def prepare_training_cornac(train, test):
+def prepare_training_bpr(train):
     return cornac.data.Dataset.from_uir(
         train.drop(DEFAULT_TIMESTAMP_COL, axis=1).itertuples(index=False), seed=SEED
     )
 
 
-
-
-
 def train_bpr(params, data):
-    model = BPR(**params)
-    with Timer() as t:
-        model.fit(data)
-    return model, t
-
-def recommend_k_bpr(model, test, train, top_k=DEFAULT_K, remove_seen=True):
-    with Timer() as t:
-        topk_scores = model.recommend_k_items(
-            train,
-            col_user=DEFAULT_USER_COL,
-            col_item=DEFAULT_ITEM_COL,
-            col_prediction=DEFAULT_PREDICTION_COL,
-            remove_seen=remove_seen,
-        )
-    return topk_scores, t
-
-def train_bivae(params, data):
-    model = cornac.models.BiVAECF(**params)
+    model = cornac.models.BPR(**params)
     with Timer() as t:
         model.fit(data)
     return model, t
 
 
-def recommend_k_bivae(model, test, train, top_k=DEFAULT_K, remove_seen=True):
+def recommend_k_bpr(model, test, train):
     with Timer() as t:
         topk_scores = predict_ranking(
             model,
@@ -355,48 +288,22 @@ def recommend_k_bivae(model, test, train, top_k=DEFAULT_K, remove_seen=True):
             usercol=DEFAULT_USER_COL,
             itemcol=DEFAULT_ITEM_COL,
             predcol=DEFAULT_PREDICTION_COL,
-            remove_seen=remove_seen,
+            remove_seen=True,
         )
     return topk_scores, t
 
 
-def prepare_training_sar(train, test):
-    return train
-
-
 def train_sar(params, data):
-    model = SAR(**params)
+    model = SARSingleNode(**params)
     model.set_index(data)
     with Timer() as t:
         model.fit(data)
     return model, t
 
 
-def recommend_k_sar(model, test, train, top_k=DEFAULT_K, remove_seen=True):
+def recommend_k_sar(model, test, train):
     with Timer() as t:
-        topk_scores = model.recommend_k_items(
-            test, top_k=top_k, remove_seen=remove_seen
-        )
-    return topk_scores, t
-
-
-def prepare_training_lightgcn(train, test):
-    return ImplicitCF(train=train, test=test)
-
-
-def train_lightgcn(params, data):
-    hparams = prepare_hparams(**params)
-    model = LightGCN(hparams, data)
-    with Timer() as t:
-        model.fit()
-    return model, t
-
-
-def recommend_k_lightgcn(model, test, train, top_k=DEFAULT_K, remove_seen=True):
-    with Timer() as t:
-        topk_scores = model.recommend_k_items(
-            test, top_k=top_k, remove_seen=remove_seen
-        )
+        topk_scores = model.recommend_k_items(test, remove_seen=True)
     return topk_scores, t
 
 
@@ -415,7 +322,7 @@ def ranking_metrics_pyspark(test, predictions, k=DEFAULT_K):
         test, predictions, k=k, relevancy_method="top_k", **COL_DICT
     )
     return {
-        "MAP": rank_eval.map(),
+        "MAP": rank_eval.map_at_k(),
         "nDCG@k": rank_eval.ndcg_at_k(),
         "Precision@k": rank_eval.precision_at_k(),
         "Recall@k": rank_eval.recall_at_k(),
@@ -433,7 +340,7 @@ def rating_metrics_python(test, predictions):
 
 def ranking_metrics_python(test, predictions, k=DEFAULT_K):
     return {
-        "MAP": map(test, predictions, k=k, **COL_DICT),
+        "MAP": map_at_k(test, predictions, k=k, **COL_DICT),
         "nDCG@k": ndcg_at_k(test, predictions, k=k, **COL_DICT),
         "Precision@k": precision_at_k(test, predictions, k=k, **COL_DICT),
         "Recall@k": recall_at_k(test, predictions, k=k, **COL_DICT),
--- examples/README.md
+++ examples/README.md
@@ -4,7 +4,7 @@ This folder contains examples and best practices, written in Jupyter notebooks, 
 
 The diagram below depicts how the best-practice examples help researchers / developers in the recommendation system development workflow.
 
-![workflow](https://raw.githubusercontent.com/recommenders-team/resources/main/images/reco_workflow.png)
+![workflow](https://recodatasets.blob.core.windows.net/images/reco_workflow.png)
 
 
 ## Summary
@@ -13,15 +13,12 @@ The following summarizes each directory of the best practice notebooks.
 
 | Directory | Runs Local | Description |
 | --- | --- | --- |
-| [00_quick_start](00_quick_start)| Yes | Quick start notebooks that demonstrate workflow of developing a recommender by using an algorithm in local environment|
-| [01_prepare_data](01_prepare_data) | Yes | Data preparation notebooks for each recommender algorithm|
-| [02_model_collaborative_filtering](02_model_collaborative_filtering) | Yes | Deep dive notebooks about model training and evaluation using collaborative filtering algorithms |
-| [02_model_content_based_filtering](02_model_content_based_filtering) | Yes |Deep dive notebooks about model training and evaluation using content-based filtering algorithms |
-| [03_evaluate](03_evaluate) | Yes | Notebooks that introduce different evaluation methods for recommenders |
-| [04_model_select_and_optimize](04_model_select_and_optimize) | Some local, some on Azure | Best practice notebooks for model tuning and selecting by using Azure Machine Learning Service and/or open source technologies |
-| [05_operationalize](05_operationalize) | No, Run on Azure | Operationalization notebooks that illustrate an end-to-end pipeline by using a recommender algorithm for a certain real-world use case scenario |
-| [06_benchmarks](06_benchmarks) | Yes | Benchmark comparison of several recommender algorithms |
-| [07_tutorials](07_tutorials) | Yes | Tutorials for using the Recommenders library |
+| [00_quick_start](./00_quick_start)| Yes | Quick start notebooks that demonstrate workflow of developing a recommender by using an algorithm in local environment|
+| [01_prepare_data](./01_prepare_data) | Yes | Data preparation notebooks for each recommender algorithm|
+| [02_model](./02_model) | Yes | Deep dive notebooks about model building by using various classical and deep learning recommender algorithms|
+| [03_evaluate](./03_evaluate) | Yes | Notebooks that introduce different evaluation methods for recommenders|
+| [04_model_select_and_optimize](04_model_select_and_optimize) | Some local, some on Azure | Best practice notebooks for model tuning and selecting by using Azure Machine Learning Service and/or open source technologies|
+| [05_operationalize](05_operationalize) | No, Run on Azure | Operationalization notebooks that illustrate an end-to-end pipeline by using a recommender algorithm for a certain real-world use case scenario|
 
 ## On-premise notebooks
 
@@ -60,13 +57,11 @@ those will be provided in the notebooks.
 ### Submit an existing notebook to Azure Machine Learning
 
  The [run_notebook_on_azureml](./run_notebook_on_azureml.ipynb) notebook provides a scaffold to directly submit an existing notebook to AzureML compute targets. After setting up a compute target and creating a run configuration, simply replace the notebook file name and submit the notebook directly. 
-
 ```python
 cfg = NotebookRunConfig(source_directory='../',
-                            notebook='examples/00_quick_start/' + NOTEBOOK_NAME,
+                            notebook='notebooks/00_quick_start/' + NOTEBOOK_NAME,
                             output_notebook='outputs/out.ipynb',
                             parameters={"MOVIELENS_DATA_SIZE": "100k", "TOP_K": 10},
                             run_config=run_config)
 ```
-
-All metrics and parameters logged with `store_metadata` will be stored on the run as tracked metrics. The initial notebook that was submitted, will be stored as an output notebook ```out.ipynb``` in the outputs tab of the Azure Portal. 
+All metrics and parameters logged with `pm.record` will be stored on the run as tracked metrics. The initial notebook that was submitted, will be stored as an output notebook ```out.ipynb``` in the outputs tab of the Azure Portal. 
