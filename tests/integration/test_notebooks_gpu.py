9fd2bccfa06e04f3d7f102ba1a35b22b41c352fb
yjw1029 <1055863721@qq.com>
2020-06-08 07:05:10 +0000

    Merge https://github.com/microsoft/recommenders into v-jinyi/add-news-reco-methods

--- tests/integration/test_notebooks_gpu.py
+++ tests/integration/test_notebooks_gpu.py
@@ -348,7 +348,7 @@ def test_naml_quickstart_integration(notebooks, epochs, seed, expected_values):
     [
         (
             5,
-            42,
+            40,
             {
                 "res_syn": {
                     "group_auc": 0.5599,
@@ -381,7 +381,6 @@ def test_lstur_quickstart_integration(notebooks, epochs, seed, expected_values):
         assert results[key]["ndcg@10"] == pytest.approx(
             value["ndcg@10"], rel=TOL, abs=ABS_TOL
         )
-
 
 @pytest.mark.gpu
 @pytest.mark.integration
