f516e924c0248bf866f9f8363c1230b373fa82c6
VisionVector <markofejzo272@gmail.com>
2020-06-25 10:57:42 +0100

    Merge pull request #1123 from Qcactus/Qcactus/add_lightgcn

--- tests/unit/test_deeprec_model.py
+++ tests/unit/test_deeprec_model.py
@@ -13,9 +13,11 @@ from reco_utils.recommender.deeprec.io.iterator import FFMTextIterator
 from reco_utils.recommender.deeprec.io.dkn_iterator import DKNTextIterator
 from reco_utils.dataset.amazon_reviews import download_and_extract, data_preprocessing
 from reco_utils.recommender.deeprec.models.sequential.sli_rec import SLI_RECModel
-from reco_utils.recommender.deeprec.models.sequential.nextitnet import NextItNetModel
 from reco_utils.recommender.deeprec.io.sequential_iterator import SequentialIterator
-from reco_utils.recommender.deeprec.io.nextitnet_iterator import NextItNetIterator
+from reco_utils.recommender.deeprec.models.graphrec.lightgcn import LightGCN
+from reco_utils.recommender.deeprec.DataModel.ImplicitCF import ImplicitCF
+from reco_utils.dataset import movielens
+from reco_utils.dataset.python_splitters import python_stratified_split
 
 
 @pytest.fixture
@@ -85,16 +87,6 @@ def test_slirec_component_definition(resource_path):
         "config",
         "sli_rec.yaml",
     )
-    yaml_file_nextitnet = os.path.join(
-        resource_path,
-        "..",
-        "..",
-        "reco_utils",
-        "recommender",
-        "deeprec",
-        "config",
-        "nextitnet.yaml",
-    )
     train_file = os.path.join(data_path, r"train_data")
 
     if not os.path.exists(train_file):
@@ -142,14 +134,40 @@ def test_slirec_component_definition(resource_path):
         yaml_file, train_num_ngs=4
     )  # confirm the train_num_ngs when initializing a SLi_Rec model.
     model = SLI_RECModel(hparams, SequentialIterator)
-    # nextitnet model
-    hparams_nextitnet = prepare_hparams(yaml_file_nextitnet, train_num_ngs=4)
-    model_nextitnet = NextItNetModel(hparams_nextitnet, NextItNetIterator)
 
     assert model.logit is not None
     assert model.update is not None
     assert model.iterator is not None
 
-    assert model_nextitnet.logit is not None
-    assert model_nextitnet.update is not None
-    assert model_nextitnet.iterator is not None
+
+@pytest.mark.gpu
+def test_lightgcn_component_definition(resource_path):
+    yaml_file = os.path.join(
+        resource_path,
+        "..",
+        "..",
+        "reco_utils",
+        "recommender",
+        "deeprec",
+        "config",
+        "lightgcn.yaml",
+    )
+
+    df = movielens.load_pandas_df(size="100k")
+    train, test = python_stratified_split(df, ratio=0.75)
+
+    data = ImplicitCF(train=train, test=test)
+
+    embed_size = 64
+    hparams = prepare_hparams(yaml_file, embed_size=embed_size)
+    model = LightGCN(hparams, data)
+
+    assert model.norm_adj is not None
+    assert model.ua_embeddings.shape == [data.n_users, embed_size]
+    assert model.ia_embeddings.shape == [data.n_items, embed_size]
+    assert model.u_g_embeddings is not None
+    assert model.pos_i_g_embeddings is not None
+    assert model.neg_i_g_embeddings is not None
+    assert model.batch_ratings is not None
+    assert model.loss is not None
+    assert model.opt is not None
