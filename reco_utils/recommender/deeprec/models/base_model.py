f708d5a417b09a9e764ae54ce56f103ca50a9578
SatyaDevNtv <tvsatyadev@gmail.com>
2020-07-15 12:51:59 +0530

    Update to be compatible with upstream's repo.

--- reco_utils/recommender/deeprec/models/base_model.py
+++ reco_utils/recommender/deeprec/models/base_model.py
@@ -455,6 +455,7 @@ class BaseModel:
                     )
 
             eval_start = time.time()
+            train_res = self.run_eval(train_file)
             eval_res = self.run_eval(valid_file)
             train_info = ",".join(
                 [
