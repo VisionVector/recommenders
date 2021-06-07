262f725f56f11c30d1f28b9df5fdbef62e39d789
VisionVector <markofejzo272@gmail.com>
2021-06-07 16:30:04 +0000

    clean repo

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ extras_require["experimental"] = [
 setup(
     name=name,
     version=version,
-    description="Microsoft Recommenders, a library of best practices and utilities for building recommendation systems",
+    description="Recommender System Utilities",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/microsoft/recommenders",
@@ -112,7 +112,7 @@ setup(
         "Operating System :: MacOS",
     ],
     extras_require=extras_require,
-    keywords="recommendations recommendation recommenders recommender system engine "
+    keywords="recommendations recommenders recommender system engine "
     "machine learning python spark gpu",
     install_requires=install_requires,
     package_dir={"reco_utils": "reco_utils"},
