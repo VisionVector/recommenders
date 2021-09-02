2c42bf8924d8ce4a107f5b7ea630d84c7548938d
Jianjie Liu <jianjl@microsoft.com>
2021-09-02 18:22:38 +0000

    Try py37

--- tests/unit/examples/test_notebooks_pyspark.py
+++ tests/unit/examples/test_notebooks_pyspark.py
@@ -8,8 +8,6 @@ try:
 except ImportError:
     pass  # disable error while collecting tests for non-notebook environments
 
-from recommenders.utils.constants import DEFAULT_RATING_COL, DEFAULT_USER_COL, DEFAULT_ITEM_COL
-
 
 @pytest.mark.notebooks
 @pytest.mark.spark
@@ -35,13 +33,7 @@ def test_data_split_runs(notebooks, output_notebook, kernel_name):
 )
 def test_als_deep_dive_runs(notebooks, output_notebook, kernel_name):
     notebook_path = notebooks["als_deep_dive"]
-    pm.execute_notebook(notebook_path, output_notebook, kernel_name=kernel_name,
-                        parameters=dict(
-                            MOVIELENS_DATA_SIZE="mock100",
-                            COL_USER=DEFAULT_USER_COL,
-                            COL_ITEM=DEFAULT_ITEM_COL,
-                            COL_RATING=DEFAULT_RATING_COL,
-                        ))
+    pm.execute_notebook(notebook_path, output_notebook, kernel_name=kernel_name)
 
 
 @pytest.mark.notebooks
@@ -58,14 +50,7 @@ def test_evaluation_runs(notebooks, output_notebook, kernel_name):
 @pytest.mark.spark
 def test_evaluation_diversity_runs(notebooks, output_notebook, kernel_name):
     notebook_path = notebooks["evaluation_diversity"]
-    pm.execute_notebook(notebook_path, output_notebook, kernel_name=kernel_name,
-                        parameters=dict(
-                            TOP_K=10,
-                            MOVIELENS_DATA_SIZE="mock100",
-                            COL_USER=DEFAULT_USER_COL,
-                            COL_ITEM=DEFAULT_ITEM_COL,
-                            COL_RATING=DEFAULT_RATING_COL,
-                        ))
+    pm.execute_notebook(notebook_path, output_notebook, kernel_name=kernel_name)
 
 
 @pytest.mark.notebooks
@@ -80,7 +65,6 @@ def test_spark_tuning(notebooks, output_notebook, kernel_name):
         output_notebook,
         kernel_name=kernel_name,
         parameters=dict(
-            MOVIELENS_DATA_SIZE="mock100",
             NUMBER_CORES="*",
             NUMBER_ITERATIONS=3,
             SUBSET_RATIO=0.5,
