3baf9e10889cc417e6343439dd74d7037dd38352
Jianjie Liu <jianjl@microsoft.com>
2021-10-13 13:29:10 +0000

    Fix missing java version

--- tests/unit/examples/test_notebooks_pyspark.py
+++ tests/unit/examples/test_notebooks_pyspark.py
@@ -11,8 +11,6 @@ except ImportError:
 from recommenders.utils.constants import DEFAULT_RATING_COL, DEFAULT_USER_COL, DEFAULT_ITEM_COL
 
 
-# This is a flaky test that can fail unexpectedly
-@pytest.mark.flaky(reruns=3, reruns_delay=2)
 @pytest.mark.notebooks
 @pytest.mark.spark
 @pytest.mark.skipif(
@@ -76,8 +74,6 @@ def test_evaluation_diversity_runs(notebooks, output_notebook, kernel_name):
                         ))
 
 
-# This is a flaky test that can fail unexpectedly
-@pytest.mark.flaky(reruns=3, reruns_delay=2)
 @pytest.mark.notebooks
 @pytest.mark.spark
 @pytest.mark.skipif(
