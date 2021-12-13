1ca1970146f1b720d5b31ab426e5ed8527ce4463
VisionVector <markofejzo272@gmail.com>
2021-12-13 14:12:35 +0100

    Merge pull request #1580 from microsoft/miguelgfierro-patch-3

--- contrib/sarplus/scala/src/main/scala/com/microsoft/sarplus/SARCacheOutputWriterFactory.scala
+++ contrib/sarplus/scala/src/main/scala/com/microsoft/sarplus/SARCacheOutputWriterFactory.scala
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 package com.microsoft.sarplus
 
 import org.apache.hadoop.mapreduce.TaskAttemptContext
