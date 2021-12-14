afaf6796cda185e4b5feb58e61eaa21774a94da5
pradnyeshjoshi <pradnyeshj94@gmail.com>
2021-12-14 19:41:07 +0000

    reran rbm notebooks with TF2

--- contrib/sarplus/scala/python/setup.py
+++ contrib/sarplus/scala/python/setup.py
@@ -1,15 +1,11 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
 from distutils.core import setup
-import os
 
 setup(
     name="pysarplus_dummy",
-    version=(Path(__file__).resolve().parent.parent.parent / "VERSION").read_text().strip(),
+    version="0.2",
     description="pysarplus dummy package to trigger spark packaging",
-    author="RecoDev Team at Microsoft",
-    author_email="recodevteam@service.microsoft.com",
+    author="Markus Cozowicz",
+    author_email="marcozo@microsoft.com",
     url="https://github.com/Microsoft/Recommenders/contrib/sarplus",
     packages=["pysarplus_dummy"],
 )
