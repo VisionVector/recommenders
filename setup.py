93c04770537d258a4fbb744b8ebcd0b6f6cfd9a2
Somasree Majumder <bishnupadamajumder32@gmail.com>
2022-02-12 09:21:40 +0530

    Changed the Readme.md file

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ extras_require = {
     "spark": [
         "databricks_cli>=0.8.6,<1",
         "pyarrow>=0.12.1,<7.0.0",
-        "pyspark>=2.4.5,<4.0.0",
+        "pyspark>=2.4.5,<3.2.0",
     ],
     "dev": [
         "black>=18.6b4,<21",
@@ -129,8 +129,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
@@ -140,7 +138,7 @@ setup(
     "machine learning python spark gpu",
     install_requires=install_requires,
     package_dir={"recommenders": "recommenders"},
-    python_requires=">=3.6, <3.10",
+    python_requires=">=3.6, <3.9",     # latest Databricks versions come with Python 3.8 installed
     packages=find_packages(where=".", exclude=["contrib", "docs", "examples", "scenarios", "tests", "tools"]),
     setup_requires=["numpy>=1.15"]
 )
