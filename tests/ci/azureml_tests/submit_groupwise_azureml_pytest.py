e3a8b40a3b57bfff652160b5538cf201e51aeef9
VisionVector <markofejzo272@gmail.com>
2023-06-07 12:35:20 +0200

    Remove bottleneck dep

--- tests/ci/azureml_tests/submit_groupwise_azureml_pytest.py
+++ tests/ci/azureml_tests/submit_groupwise_azureml_pytest.py
@@ -196,6 +196,7 @@ def create_run_config(
     )
     conda_dep = CondaDependencies()
     conda_dep.add_conda_package(conda_pkg_python)
+    conda_dep.add_pip_package(whl_url)
     conda_dep.add_pip_package(
         "pymanopt@https://github.com/pymanopt/pymanopt/archive/fb36a272cdeecb21992cfd9271eb82baafeb316d.zip"
     )
@@ -204,15 +205,15 @@ def create_run_config(
     if add_gpu_dependencies and add_spark_dependencies:
         conda_dep.add_channel("conda-forge")
         conda_dep.add_conda_package(conda_pkg_jdk)
-        conda_dep.add_pip_package("recommenders[dev,examples,spark,gpu]@" + whl_url)
+        conda_dep.add_pip_package("recommenders[dev,examples,spark,gpu]")
     elif add_gpu_dependencies:
-        conda_dep.add_pip_package("recommenders[dev,examples,gpu]@" + whl_url)
+        conda_dep.add_pip_package("recommenders[dev,examples,gpu]")
     elif add_spark_dependencies:
         conda_dep.add_channel("conda-forge")
         conda_dep.add_conda_package(conda_pkg_jdk)
-        conda_dep.add_pip_package("recommenders[dev,examples,spark]@" + whl_url)
+        conda_dep.add_pip_package("recommenders[dev,examples,spark]")
     else:
-        conda_dep.add_pip_package("recommenders[dev,examples]@" + whl_url)
+        conda_dep.add_pip_package("recommenders[dev,examples]")
 
     run_azuremlcompute.environment.python.conda_dependencies = conda_dep
     return run_azuremlcompute
