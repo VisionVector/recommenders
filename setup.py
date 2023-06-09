393555c5e17a27cc64f5e66de2f75e2a76d82599
Simon Zhao <43029286+simonzhaoms@users.noreply.github.com>
2023-06-09 11:15:56 +0800

    Merge in staging

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import site
 import sys
 import time
 
-# workround for enabling editable user pip installs
+# workaround for enabling editable user pip installs
 site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 # version
@@ -27,58 +27,52 @@ if HASH is not None:
     version += ".post" + str(int(time.time()))
 
 install_requires = [
-    "numpy>=1.19",  # 1.19 required by tensorflow 2.6
-    "pandas>1.0.3,<2",
-    "scipy>=1.0.0,<2",
-    "tqdm>=4.31.1,<5",
-    "matplotlib>=2.2.2,<4",
-    "scikit-learn>=0.22.1,<1.0.3",
-    "numba>=0.38.1,<1",
-    "lightfm>=1.15,<2",
-    "lightgbm>=2.2.1",
-    "memory_profiler>=0.54.0,<1",
-    "nltk>=3.4,<4",
-    "seaborn>=0.8.1,<1",
-    "transformers>=2.5.0,<5",
-    "bottleneck>=1.2.1,<2",
-    "category_encoders>=1.3.0,<2",
-    "jinja2>=2,<3.1",
-    "requests>=2.0.0,<3",
-    "cornac>=1.1.2,<1.15.2;python_version<='3.7'",
-    "cornac>=1.15.2,<2;python_version>='3.8'", # After 1.15.2, Cornac requires python 3.8
-    "retrying>=1.3.3",
-    "pandera[strategies]>=0.6.5",  # For generating fake datasets
-    "scikit-surprise>=1.0.6",
-    "scrapbook>=0.5.0,<1.0.0",
+    "pandas>1.5.2,<2.1",  # requires numpy
+    "scikit-learn>=1.1.3,<2",  # requires scipy
+    "numba>=0.57.0,<1",
+    "lightfm>=1.17,<2",
+    "lightgbm>=3.3.2,<4",
+    "memory-profiler>=0.61.0,<1",
+    "nltk>=3.8.1,<4",  # requires tqdm
+    "seaborn>=0.12.0,<1",  # requires matplotlib
+    "transformers>=4.26.0,<5",  # requires pyyaml, tqdm
+    "bottleneck>=1.3.7,<2",
+    "category-encoders>=2.6.0,<3",
+    "jinja2>=3.1.0,<3.2",
+    "cornac>=1.15.2,<2",  # requires tqdm
+    "retrying>=1.3.4",
+    "pandera[strategies]>=0.15.0",  # For generating fake datasets
+    "scikit-surprise>=1.1.3",
+    "scrapbook>=0.5.0,<1.0.0",  # requires tqdm, papermill
 ]
 
 # shared dependencies
 extras_require = {
     "examples": [
-        "hyperopt>=0.1.2,<1",
-        "ipykernel>=4.6.1,<7",
-        "jupyter>=1,<2",
-        "locust>=1,<2",
-        "papermill>=2.1.2,<3",
+        "hyperopt>=0.2.7,<1",
+        "notebook>=6.5.4,<8",  # requires jupyter, ipykernel
+        "locust>=2.15.1,<3",
     ],
     "gpu": [
-        "nvidia-ml-py3>=7.352.0",
+        "nvidia-ml-py3>=11.510.69",
         # TensorFlow compiled with CUDA 11.2, cudnn 8.1
         "tensorflow~=2.6.1;python_version=='3.6'",
         "tensorflow~=2.7.0;python_version>='3.7'",
         "tf-slim>=1.1.0",
-        "torch>=1.8",  # for CUDA 11 support
-        "fastai>=1.0.46,<2",
+        "torch>=2.0.1",
+        "fastai>=2.7.11,<3",
     ],
     "spark": [
-        "pyarrow>=0.12.1,<7.0.0",
-        "pyspark>=2.4.5,<3.3.0",
+        "databricks-cli>=0.17.7,<1",
+        "pyarrow>=10.0.1",
+        "pyspark>=3.0.1,<=3.4.0",
     ],
     "dev": [
-        "black>=18.6b4,<21",
-        "pytest>=3.6.4",
-        "pytest-cov>=2.12.1",
-        "pytest-mock>=3.6.1",  # for access to mock fixtures in pytest
+        "black>=23.3.0,<24",
+        "pytest>=7.2.1",
+        "pytest-cov>=4.1.0",
+        "pytest-mock>=3.10.0",  # for access to mock fixtures in pytest
+        "pytest-rerunfailures>=11.1.2",  # to mark flaky tests
     ],
 }
 # for the brave of heart
@@ -134,9 +128,6 @@ setup(
     install_requires=install_requires,
     package_dir={"recommenders": "recommenders"},
     python_requires=">=3.6, <3.10",
-    packages=find_packages(
-        where=".",
-        exclude=["contrib", "docs", "examples", "scenarios", "tests", "tools"],
-    ),
-    setup_requires=["numpy>=1.19"],
+    packages=find_packages(where=".", exclude=["contrib", "docs", "examples", "scenarios", "tests", "tools"]),
+    setup_requires=["numpy>=1.15"]
 )
