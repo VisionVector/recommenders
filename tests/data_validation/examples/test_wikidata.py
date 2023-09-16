a3a5dbb6697cfaf64e314109c77632bbe8a66dbf
VisionVector <markofejzo272@gmail.com>
2023-09-16 18:21:39 +0200

    functional CPU

--- tests/data_validation/examples/test_wikidata.py
+++ tests/data_validation/examples/test_wikidata.py
@@ -3,32 +3,15 @@
 
 
 import pytest
+import papermill as pm
+import scrapbook as sb
 
-from recommenders.utils.notebook_utils import execute_notebook, read_notebook
 
-
-@pytest.mark.skip(reason="Wikidata API is unstable")
 @pytest.mark.notebooks
-def test_wikidata_runs(notebooks, output_notebook, kernel_name, tmp):
-    notebook_path = notebooks["wikidata_knowledge_graph"]
-    MOVIELENS_SAMPLE_SIZE = 5
-    execute_notebook(
-        notebook_path,
-        output_notebook,
-        kernel_name=kernel_name,
-        parameters=dict(
-            MOVIELENS_DATA_SIZE="100k",
-            MOVIELENS_SAMPLE=True,
-            MOVIELENS_SAMPLE_SIZE=MOVIELENS_SAMPLE_SIZE,
-        ),
-    )
-
-
 @pytest.mark.skip(reason="Wikidata API is unstable")
-@pytest.mark.notebooks
 def test_wikidata_values(notebooks, output_notebook, kernel_name):
     notebook_path = notebooks["wikidata_knowledge_graph"]
-    execute_notebook(
+    pm.execute_notebook(
         notebook_path,
         output_notebook,
         kernel_name=kernel_name,
@@ -36,7 +19,9 @@ def test_wikidata_values(notebooks, output_notebook, kernel_name):
             MOVIELENS_DATA_SIZE="100k", MOVIELENS_SAMPLE=True, MOVIELENS_SAMPLE_SIZE=5
         ),
     )
-    results = read_notebook(output_notebook)
+    results = sb.read_notebook(output_notebook).scraps.dataframe.set_index("name")[
+        "data"
+    ]
 
     # NOTE: The return number should be always 5, but sometimes we get less because wikidata is unstable
     assert results["length_result"] >= 1
