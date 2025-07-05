2c88413f7dbb1c9eef94697814e890643b042fd4
Jose Miguel Arrieta <jmarrietar@gmail.com>
2025-07-05 21:32:00 +0000

    Small change to make it clear

--- examples/06_benchmarks/benchmark_utils.py
+++ examples/06_benchmarks/benchmark_utils.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import pandas as pd
 from tempfile import TemporaryDirectory
+import surprise
 import cornac
 
 try:
@@ -17,10 +18,6 @@ try:
     from fastai.collab import collab_learner, CollabDataLoaders
 except ImportError:
     pass  # skip this import if we are not in a GPU environment
-try:
-    import surprise # Put SVD surprise back in core deps when #2224 is fixed
-except:
-    pass 
 
 from recommenders.utils.timer import Timer
 from recommenders.utils.constants import (
